23c3b166f136cdcdcf1c9cb8ccd140e7f8e71680
Neon-jeff <omaksteejay@gmail.com>
2025-03-27 06:48:47 +0100

    feat:created time to trip details

--- api/models.py
+++ api/models.py
@@ -7,6 +7,7 @@ class Trip(models.Model):
     current_cycle_used = models.IntegerField()
     pickup_location = models.JSONField()
     dropoff_location = models.JSONField()
+    created = models.DateTimeField(auto_now_add=True,null=True,blank=True)
 
     def __str__(self) -> str:
         return f'{self.dropoff_location['city']} Trip'
